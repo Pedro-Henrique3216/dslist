a51ae3730295c46d7416d45ffacb6fbfe586b3a1
Pedro Henrique <ph6704938@gmail.com>
2023-07-11 22:28:57 -0300

    domain model

---
+++ src/main/java/com/devsuperior/dslist/entities/Belonging.java
@@ -0,0 +1,28 @@
+package com.devsuperior.dslist.entities;
+
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "tb_belonging")
+public class Belonging {
+
+
+    @EmbeddedId
+    private BelongingPK id = new BelongingPK();
+    private Integer position;
+
+    public Belonging(Game game, GameList list, Integer position){
+       id.setGame(game);
+       id.setList(list);
+       this.position = position;
+
+    }
+}
